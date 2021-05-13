5834ce05e01b123917521bfd22df7d2c242350e2
Martynas Bagdonas <martbgd@gmail.com>
2021-05-13 11:59:42 +0300

    Improve automatic highlight unlinking, fix corruption on Enter

--- src/core/plugins/highlight.js
+++ src/core/plugins/highlight.js
@@ -1,133 +1,27 @@
 import { Plugin, PluginKey, TextSelection } from 'prosemirror-state';
-import { fromHTML, schema } from '../schema';
-import { toggleMark } from 'prosemirror-commands';
-import { formatCitation, randomString, SetAttrsStep } from '../utils';
-import { Fragment, NodeRange, Slice } from 'prosemirror-model';
-import { TextNode } from 'prosemirror-model/src/node';
-import { liftTarget, ReplaceAroundStep, ReplaceStep } from 'prosemirror-transform';
-
-window.TextSelection = TextSelection;
-
-
-function textRange(node, from, to) {
-	const range = document.createRange();
-	range.setEnd(node, to == null ? node.nodeValue.length : to);
-	range.setStart(node, from || 0);
-	return range;
-}
-
-function singleRect(object, bias) {
-	const rects = object.getClientRects();
-	return !rects.length ? object.getBoundingClientRect() : rects[bias < 0 ? 0 : rects.length - 1];
-}
-
-function coordsAtPos(view, pos, end = false) {
-	const { node, offset } = view.docView.domFromPos(pos);
-	let side;
-	let rect;
-	if (node.nodeType === 3) {
-		if (end && offset < node.nodeValue.length) {
-			rect = singleRect(textRange(node, offset - 1, offset), -1);
-			side = 'right';
-		}
-		else if (offset < node.nodeValue.length) {
-			rect = singleRect(textRange(node, offset, offset + 1), -1);
-			side = 'left';
-		}
-	}
-	else if (node.firstChild) {
-		if (offset < node.childNodes.length) {
-			const child = node.childNodes[offset];
-			rect = singleRect(child.nodeType === 3 ? textRange(child) : child, -1);
-			side = 'left';
-		}
-		if ((!rect || rect.top === rect.bottom) && offset) {
-			const child = node.childNodes[offset - 1];
-			rect = singleRect(child.nodeType === 3 ? textRange(child) : child, 1);
-			side = 'right';
-		}
-	}
-	else {
-		rect = node.getBoundingClientRect();
-		side = 'left';
-	}
-
-	const x = rect[side];
-	return {
-		top: rect.top,
-		bottom: rect.bottom,
-		left: x,
-		right: x
-	};
-}
-
-function getNode(state) {
-	const { $from, $to, $cursor } = state.selection;
-
-	let nodes = [];
-	state.doc.nodesBetween($from.pos, $to.pos, (parentNode, parentPos) => {
-		parentNode.forEach((node, offset, index) => {
-			let absolutePos = parentPos + offset + 1;
-			if (node.type.name === 'highlight') {
-				if ($from.pos > absolutePos + 1 && $to.pos < absolutePos + node.nodeSize - 1) {
-					nodes.push({ pos: absolutePos, node, parent: parentNode, index });
-				}
-			}
-		});
-	});
-	if (nodes.length === 1) {
-		return nodes[0];
-	}
-	return null;
-}
+import { Fragment, Slice } from 'prosemirror-model';
+import { ReplaceAroundStep, ReplaceStep } from 'prosemirror-transform';
+import { schema } from '../schema';
+import { getSingleSelectedNode } from '../commands';
+import { formatCitation } from '../utils';
 
 class Highlight {
 	constructor(state, options) {
 		this.options = options;
-		this.popup = {
-			isActive: false
-		};
-		// this.onOpenURL = options.onOpenURL;
+		this.popup = { active: false };
 	}
 
 	update(state, oldState) {
 		if (!this.view) {
-			this.popup = { ...this.popup, isActive: false };
 			return;
 		}
 
-		let node = getNode(state);
-
-		let pos;
-		let index;
-		let parent;
-		if (node) {
-			pos = node.pos;
-			index = node.index;
-			parent = node.parent;
-			node = node.node;
-		}
-
-		if (node) {
-			let { from, to } = state.selection;
-
-			// TODO: Should be withing the bounds of the highlight
-
-			// These are in screen coordinates
-			// We can't use EditorView.cordsAtPos here because it can't handle linebreaks correctly
-			// See: https://github.com/ProseMirror/prosemirror-view/pull/47
-			let start = coordsAtPos(this.view, from);
-			let end = coordsAtPos(this.view, to, true);
-			let isMultiline = start.top !== end.top;
-			let left = isMultiline ? start.left : start.left + (end.left - start.left) / 2;
-
+		let nodeData = getSingleSelectedNode(state, schema.nodes.highlight, true);
+		if (nodeData) {
+			let { node, pos, index, parent } = nodeData;
 			let dom = this.view.nodeDOM(pos);
 			let rect = dom.getBoundingClientRect();
-
-			let next = this.view.state.doc.resolve(pos);
-
 			let citation = null;
-
 			// TODO: Add part of this into try / catch to make sure bad data doesn't prevent showing popup here and in other places
 			if (node.attrs.annotation.citationItem) {
 				for (let i = index + 1; i < parent.childCount; i++) {
@@ -150,24 +44,16 @@ class Highlight {
 			}
 
 			this.popup = {
-				isActive: true,
-				left,
-				top: start.top,
-				bottom: end.bottom,
-				isMultiline,
-				pos: from,
+				active: true,
 				rect,
-				enableAddCitation: !citation && !!node.attrs.annotation.citationItem,
+				canAddCitation: !citation && !!node.attrs.annotation.citationItem,
 				open: this.open.bind(this),
 				unlink: this.unlink.bind(this),
 				addCitation: this.addCitation.bind(this)
 			};
 			return;
 		}
-
-		this.popup = {
-			isActive: false
-		};
+		this.popup = { active: false };
 	}
 
 	open() {
@@ -218,7 +104,7 @@ class Highlight {
 	}
 
 	destroy() {
-		this.popup = { ...this.popup, isActive: false };
+		this.popup = { active: false };
 	}
 }
 

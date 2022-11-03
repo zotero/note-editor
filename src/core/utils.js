zotero/note-editor

commit 093ff40b1c011579e98e5266c9762979bcd8059d

Fix typo in source

src/core/utils.js
@@ -290,7 +290,7 @@ export class SetAttrsStep extends Step {
 	apply(doc) {
 		let target = doc.nodeAt(this.pos);
 		if (!target) return StepResult.fail('No node at given position');
-		let newNode = target.type.create(this.attrs, Fragment.emtpy, target.marks);
+		let newNode = target.type.create(this.attrs, Fragment.empty, target.marks);
 		let slice = new Slice(Fragment.from(newNode), 0, target.isLeaf ? 0 : 1);
 		return StepResult.fromReplace(doc, this.pos, this.pos + 1, slice);
 	}

commit 40db31027f913b4f47e7f74c62c2a87d9fb5ad38

Fix regression: No longer possible to add images
This is a regression introduced in 093ff40. Previously, because of a
typo, argument was resolving to `null`. Changing it to `Fragment.empty`
actually triggers error in certain cases, such as inserting an image.

src/core/utils.js
@@ -290,7 +290,7 @@ export class SetAttrsStep extends Step {
 	apply(doc) {
 		let target = doc.nodeAt(this.pos);
 		if (!target) return StepResult.fail('No node at given position');
-		let newNode = target.type.create(this.attrs, Fragment.empty, target.marks);
+		let newNode = target.type.create(this.attrs, null, target.marks);
 		let slice = new Slice(Fragment.from(newNode), 0, target.isLeaf ? 0 : 1);
 		return StepResult.fromReplace(doc, this.pos, this.pos + 1, slice);
 	}